9912669ec4e88dfd9de0d729d7171499421007cd
Andrew Nesbitt <andrewnez@gmail.com>
2015-10-30 15:59:15 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: ruby dispatch.rb
---
+++ dispatch.rb
@@ -0,0 +1,51 @@
+require 'em-eventsource'
+require 'sidekiq'
+
+EM.run do
+  source = EM::EventSource.new("http://github-firehose.herokuapp.com/events")
+
+  source.error do |error|
+    puts "error #{error}"
+  end
+
+  source.on "event" do |message|
+    data = JSON.parse(message)
+    case data['type']
+    when 'WatchEvent'
+      puts 'new star'
+    when 'PublicEvent'
+      Sidekiq::Client.push('class' => 'GithubCreateWorker', 'args' => [data['repo']['name'], nil])
+    when 'ReleaseEvent'
+      puts 'new release'
+    when 'CreateEvent'
+      thing = data['payload']['ref_type']
+      if thing == 'tag'
+        puts 'new tag'
+      elsif thing == 'repository'
+        Sidekiq::Client.push('class' => 'GithubCreateWorker', 'args' => [data['repo']['name'], nil])
+      end
+    end
+  end
+
+  source.start
+end
+
+# WatchEvent => star
+# PublicEvent => new repo
+# ReleaseEvent => new release
+# CreateEvent => new (repo/branch/tag)
+
+# PushEvent
+# CommitCommentEvent
+# CreateEvent
+# DeleteEvent
+# ForkEvent
+# GollumEvent
+# IssueCommentEvent
+# IssuesEvent
+# MemberEvent
+# PublicEvent
+# PullRequestEvent
+# PullRequestReviewCommentEvent
+# ReleaseEvent
+# WatchEvent
