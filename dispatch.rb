2fb28bca765ddbd9927006804b8baf1c833d1782
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-05 22:44:10 +0100

    handle more issue and pull request events

--- dispatch.rb
+++ dispatch.rb
@@ -25,13 +25,13 @@ EM.run do
       Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubUpdateWorker', 'args' => [data['repo']['name'], nil])
     when 'IssuesEvent'
       case data['payload']['action']
-      when 'opened', 'closed', 'reopened'
+      when 'opened', 'closed', 'reopened', 'labeled', 'unlabeled', 'edited'
         p "Issue #{data['payload']['action']} #{data['repo']['name']}"
         Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubUpdateWorker', 'args' => [data['repo']['name'], nil])
       end
     when 'PullRequestEvent'
       case data['payload']['action']
-      when 'opened', 'closed', 'reopened'
+      when 'opened', 'closed', 'reopened', 'labeled', 'unlabeled', 'edited'
         p "Pull Request #{data['payload']['action']} #{data['repo']['name']}"
         # Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubUpdateWorker', 'args' => [data['repo']['name'], nil])
       end
