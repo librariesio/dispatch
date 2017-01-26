34c445c3713e031a65919c0f48d4ecafcac01425
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-26 12:51:04 +0000

    Rename some worker classes

--- dispatch.rb
+++ dispatch.rb
@@ -19,7 +19,7 @@ EM.run do
       Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubStarWorker', 'args' => [data['repo']['name'], nil])
     when 'PublicEvent'
       p 'pubic'
-      Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubCreateWorker', 'args' => [data['repo']['name'], nil])
+      Sidekiq::Client.push('queue' => 'low', 'class' => 'CreateRepositoryWorker', 'args' => [data['repo']['name'], nil])
     when 'ReleaseEvent'
       p 'new release'
       Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubUpdateWorker', 'args' => [data['repo']['name'], nil])
@@ -30,25 +30,25 @@ EM.run do
       case data['payload']['action']
       when 'opened', 'closed', 'reopened', 'labeled', 'unlabeled', 'edited'
         p "Issue #{data['payload']['action']} #{data['repo']['name']}"
-        Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubIssueWorker', 'args' => [data['repo']['name'], data['payload']['issue']['number'],nil])
+        Sidekiq::Client.push('queue' => 'low', 'class' => 'IssueWorker', 'args' => [data['repo']['name'], data['payload']['issue']['number'],nil])
       end
     when 'PullRequestEvent'
       case data['payload']['action']
       when 'opened', 'closed', 'reopened', 'labeled', 'unlabeled', 'edited'
         p "Pull Request #{data['payload']['action']} #{data['repo']['name']}"
-        Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubIssueWorker', 'args' => [data['repo']['name'], data['payload']['pull_request']['number'], nil])
+        Sidekiq::Client.push('queue' => 'low', 'class' => 'IssueWorker', 'args' => [data['repo']['name'], data['payload']['pull_request']['number'], nil])
       end
     when 'IssueCommentEvent'
       p 'new issue comment'
-      Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubIssueWorker', 'args' => [data['repo']['name'], data['payload']['issue']['number'],nil])
+      Sidekiq::Client.push('queue' => 'low', 'class' => 'IssueWorker', 'args' => [data['repo']['name'], data['payload']['issue']['number'],nil])
     when 'CreateEvent'
       thing = data['payload']['ref_type']
       if thing == 'tag'
         p 'new tag'
-        Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubTagWorker', 'args' => [data['repo']['name'], nil])
+        Sidekiq::Client.push('queue' => 'low', 'class' => 'TagWorker', 'args' => [data['repo']['name'], nil])
       elsif thing == 'repository'
         p 'new repo'
-        Sidekiq::Client.push('queue' => 'low', 'class' => 'GithubCreateWorker', 'args' => [data['repo']['name'], nil])
+        Sidekiq::Client.push('queue' => 'low', 'class' => 'CreateRepositoryWorker', 'args' => [data['repo']['name'], nil])
       end
     end
   end
