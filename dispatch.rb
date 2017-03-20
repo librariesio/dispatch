2e6fe3adbd6600ad4e6b84ddd7bdb43ea93f202d
Andrew Nesbitt <andrewnez@gmail.com>
2017-03-20 14:49:01 +0000

    Refactor parsing into a method on EventSender

--- dispatch.rb
+++ dispatch.rb
@@ -6,7 +6,67 @@ class EventSender
     @connection = EventMachine::HttpRequest.new(ENV["EVENT_HOOK_URL"])
   end
 
+  def parse(message)
+    data = JSON.parse(message)
+
+    case data['type']
+    when 'RepositoryEvent'
+      send_event(
+        'repository', {
+          "repository" => { "full_name" => data['repo']['name'] }
+      })
+    when 'WatchEvent'
+      send_event('watch', {
+        "repository" => { "full_name" => data['repo']['name'] }
+      })
+    when 'PublicEvent'
+      send_event(
+        'public', {
+          "repository" => { "full_name" => data['repo']['name'] }
+      })
+    when 'ReleaseEvent'
+      send_event(
+        'release', {
+          "repository" => { "full_name" => data['repo']['name'] }
+      })
+    when 'ForkEvent'
+      # send_event(
+      #   'repository', {
+      #     "repository" => { "full_name" => data['repo']['name'] }
+      # })
+    when 'IssuesEvent'
+      send_event(
+        'issues', {
+          "action" => data['payload']['action'],
+          "issue" => { "number" => data['payload']['issue']['number'] },
+          "repository" => { 'id' => data['repo']['id'], "full_name" => data['repo']['name'] },
+          'sender' => { 'id' => data['actor']['id'] }
+      })
+    when 'PullRequestEvent'
+      send_event(
+        'pull_request', {
+          "action" => data['payload']['action'],
+          "pull_request" => { "number" => data['payload']['pull_request']['number'] },
+          "repository" => { 'id' => data['repo']['id'], "full_name" => data['repo']['name'] },
+          'sender' => { 'id' => data['actor']['id'] }
+      })
+    when 'IssueCommentEvent'
+      send_event(
+        'issue_comment', {
+          "repository" => { "full_name" => data['repo']['name'] },
+          "issue" => { "number" => data['payload']['issue']['number'] }
+        }
+      )
+    when 'CreateEvent'
+      send_event('create', {
+        'ref_type' => data['payload']['ref_type'],
+        "repository" => { "full_name" => data['repo']['name'] }
+      })
+    end
+  end
+
   def send_event(name, params = {})
+    puts "Sending '#{name}' event"
     @connection.post({
       body: JSON.dump(params),
       head: {
@@ -18,77 +78,9 @@ class EventSender
 end
 
 EM.run do
-  source = EM::EventSource.new(ENV["FIREHOSE_URL"])
   sender = EventSender.new
-
-  source.error do |error|
-    puts "error #{error}"
-  end
-
-  source.on "event" do |message|
-    data = JSON.parse(message)
-
-    case data['type']
-    when 'RepositoryEvent'
-      p 'RepositoryEvent'
-      sender.send_event(
-        'repository', {
-          "repository" => { "full_name" => data['repo']['name'] }
-      })
-    when 'WatchEvent'
-      p 'star'
-      sender.send_event('watch', {
-        "repository" => { "full_name" => data['repo']['name'] }
-      })
-    when 'PublicEvent'
-      p 'public'
-      sender.send_event(
-        'public', {
-          "repository" => { "full_name" => data['repo']['name'] }
-      })
-    when 'ReleaseEvent'
-      p 'new release'
-      sender.send_event(
-        'release', {
-          "repository" => { "full_name" => data['repo']['name'] }
-      })
-    when 'ForkEvent'
-      p 'new fork'
-      # Sidekiq::Client.push('queue' => 'low', 'class' => 'CreateRepositoryWorker', 'args' => ['GitHub', data['repo']['name'], nil])
-    when 'IssuesEvent'
-      p "Issue #{data['payload']['action']} #{data['repo']['name']}"
-      sender.send_event(
-        'issues', {
-          "action" => data['payload']['action'],
-          "issue" => { "number" => data['payload']['issue']['number'] },
-          "repository" => { 'id' => data['repo']['id'], "full_name" => data['repo']['name'] },
-          'sender' => { 'id' => data['actor']['id'] }
-      })
-    when 'PullRequestEvent'
-      p "Pull Request #{data['payload']['action']} #{data['repo']['name']}"
-      sender.send_event(
-        'pull_request', {
-          "action" => data['payload']['action'],
-          "pull_request" => { "number" => data['payload']['pull_request']['number'] },
-          "repository" => { 'id' => data['repo']['id'], "full_name" => data['repo']['name'] },
-          'sender' => { 'id' => data['actor']['id'] }
-      })
-    when 'IssueCommentEvent'
-      p 'new issue comment'
-      sender.send_event(
-        'issue_comment', {
-          "repository" => { "full_name" => data['repo']['name'] },
-          "issue" => { "number" => data['payload']['issue']['number'] }
-        }
-      )
-    when 'CreateEvent'
-      p 'new create'
-      sender.send_event('create', {
-        'ref_type' => data['payload']['ref_type'],
-        "repository" => { "full_name" => data['repo']['name'] }
-      })
-    end
-  end
-
+  source = EM::EventSource.new(ENV["FIREHOSE_URL"])
+  source.on "event", &sender.method(:parse)
+  source.error {|e| puts "error #{e}" }
   source.start
 end
