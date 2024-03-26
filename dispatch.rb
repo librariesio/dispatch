39a25222722bcf39fa02877526a4cb1d4bf559b5
John Bintz <johnbintz@tidelift.com>
2024-03-26 10:40:44 -0400

    These are separate watchers

--- dispatch.rb
+++ dispatch.rb
@@ -15,18 +15,18 @@ EM.run do
   # Watcher
 
   # Github event processing
-  event_sender = EventSender.new(ENV['GITHUB_HOOK_URL'])
-  github = GithubProcessor.new(event_sender)
+  github_event_sender = EventSender.new(ENV['GITHUB_HOOK_URL'])
+  github = GithubProcessor.new(github_event_sender)
   source = EM::EventSource.new(ENV['FIREHOSE_URL'])
   source.on 'event', &github.method(:process)
   source.error { |e| puts "error #{e}" }
   source.start
 
   # Package manager monitoring
-  event_sender = EventSender.new(ENV['WATCHER_HOOK_URL'])
+  watcher_event_sender = EventSender.new(ENV['WATCHER_HOOK_URL'])
   cache = MemcachedCache.client
   names_cache = ProcessedPackageNamesCache.new(cache:)
-  watcher = Watcher.new(event_sender:, names_cache:)
+  watcher = Watcher.new(event_sender: watcher_event_sender, names_cache:)
   EventMachine.add_periodic_timer(120) { watcher.call }
   watcher.call
 end
