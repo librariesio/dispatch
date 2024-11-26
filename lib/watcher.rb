11ef316b5ece3b61022748a6655c76bff516382c
Jeremy Katz <jeremy@tidelift.com>
2024-11-26 22:48:09 +0000

    Moved these to depper

--- lib/watcher.rb
+++ lib/watcher.rb
@@ -2,36 +2,36 @@
 
 class Watcher
   PACKAGE_MANAGER_SERVICES = {
-    cpan: PackageManagerService.new(
-      url: 'https://fastapi.metacpan.org/v1/release/_search?q=status:latest&fields=distribution&sort=date:desc&size=100',
-      platform: 'CPAN',
-      request_parser: RequestParser::Json,
-      request_body_processor: RequestBodyProcessor::Cpan
-    ),
-    hex_inserted_at: PackageManagerService.new(
-      url: 'https://hex.pm/api/packages?sort=inserted_at',
-      platform: 'Hex',
-      request_parser: RequestParser::Json,
-      request_body_processor: RequestBodyProcessor::Hex
-    ),
-    hex_updated_at: PackageManagerService.new(
-      url: 'https://hex.pm/api/packages?sort=updated_at',
-      platform: 'Hex',
-      request_parser: RequestParser::Json,
-      request_body_processor: RequestBodyProcessor::Hex
-    ),
-    hackage: PackageManagerService.new(
-      url: 'https://hackage.haskell.org/packages/recent.rss',
-      platform: 'Hackage',
-      request_parser: RequestParser::Rss,
-      request_body_processor: RequestBodyProcessor::Hackage
-    ),
-    pub: PackageManagerService.new(
-      url: 'https://pub.dartlang.org/feed.atom',
-      platform: 'Pub',
-      request_parser: RequestParser::Rss,
-      request_body_processor: RequestBodyProcessor::Pub
-    ),
+    # cpan: PackageManagerService.new(
+    #   url: 'https://fastapi.metacpan.org/v1/release/_search?q=status:latest&fields=distribution&sort=date:desc&size=100',
+    #   platform: 'CPAN',
+    #   request_parser: RequestParser::Json,
+    #   request_body_processor: RequestBodyProcessor::Cpan
+    # ),
+    # hex_inserted_at: PackageManagerService.new(
+    #   url: 'https://hex.pm/api/packages?sort=inserted_at',
+    #   platform: 'Hex',
+    #   request_parser: RequestParser::Json,
+    #   request_body_processor: RequestBodyProcessor::Hex
+    # ),
+    # hex_updated_at: PackageManagerService.new(
+    #   url: 'https://hex.pm/api/packages?sort=updated_at',
+    #   platform: 'Hex',
+    #   request_parser: RequestParser::Json,
+    #   request_body_processor: RequestBodyProcessor::Hex
+    # ),
+    # hackage: PackageManagerService.new(
+    #   url: 'https://hackage.haskell.org/packages/recent.rss',
+    #   platform: 'Hackage',
+    #   request_parser: RequestParser::Rss,
+    #   request_body_processor: RequestBodyProcessor::Hackage
+    # ),
+    # pub: PackageManagerService.new(
+    #   url: 'https://pub.dartlang.org/feed.atom',
+    #   platform: 'Pub',
+    #   request_parser: RequestParser::Rss,
+    #   request_body_processor: RequestBodyProcessor::Pub
+    # ),
     # Service returning 403 forbidden as of 2024-03-26
     # mvnrepository: PackageManagerService.new(
     #  url: 'https://mvnrepository.com/feeds/rss2.0.xml',
